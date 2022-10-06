ac1161f3cfc74101ee9d0a57700d32b4d4f8c5a1
ayrton <ayrton.parkinson1@gmail.com>
2022-10-06 07:34:43 -0400

    Move BrowserRouter to index.js to fix error with navbar links

--- src/components/RouteSwitch.js
+++ src/components/RouteSwitch.js
@@ -1,16 +1,14 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Home from './home/Home';
 import Shop from './shop/Shop';
 
 function RouteSwitch() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/shop" element={<Shop />} />
-      </Routes>
-    </BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/shop" element={<Shop />} />
+    </Routes>
   );
 }
 
