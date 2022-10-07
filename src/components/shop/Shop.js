658e63550f716e7a77512788f0c98210ceb92e2e
ayrton <ayrton.parkinson1@gmail.com>
2022-10-07 07:47:31 -0400

    Add ability to add product to cart

--- src/components/shop/Shop.js
+++ src/components/shop/Shop.js
@@ -1,9 +1,57 @@
-import React from 'react';
+import React, { useState } from 'react';
+import ProductCard from './ProductCard';
+import uniqid from 'uniqid';
 
 function Shop() {
+  const products = [
+    {
+      name: 'Hat',
+      image: 'hat.png',
+      price: '24.99',
+      id: uniqid(),
+    },
+  ];
+
+  const [cart, setCart] = useState([]);
+
+  const addToCart = (cartObject) => {
+    let newItem = true;
+    const updatedCart = [];
+
+    cart.forEach((item) => {
+      if (item.name === cartObject.name) {
+        newItem = false;
+        updatedCart.push(
+          {
+            ...item,
+            quantity: item.quantity + cartObject.quantity,
+          }
+        );
+      } else {
+        updatedCart.push(item);
+      }
+    });
+
+    if (newItem) { updatedCart.push(cartObject); }
+
+    console.log(updatedCart);
+
+    setCart(updatedCart);
+  }
+
   return (
     <div>
-      
+      <ul>
+        { products.map((product) => (
+          <ProductCard
+            name = {product.name}
+            image={product.image}
+            price={product.price}
+            handleSubmit={addToCart}
+            key={product.id}
+          />
+        ))}
+      </ul>
     </div>
   );
 }
