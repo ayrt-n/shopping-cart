481bab6086f51bf2e539a3e5c12929457176a6c9
ayrton <ayrton.parkinson1@gmail.com>
2022-10-08 07:56:13 -0400

    Add product images to home page

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import image1 from './images/stussy-home-1.webp';
+import image2 from './images/stussy-home-2.webp';
+import image3 from './images/stussy-home-3.webp';
+import image4 from './images/stussy-home-4.webp';
+import image5 from './images/stussy-home-5.webp';
+import '../../styles/Home.css';
 
 function Home() {
   return (
-    <div>
-      
+    <div className="Home">
+      <div className="Home-showcase-container">
+        <img src={image1} alt="Man wearing black stussy shirt"/>
+        <img src={image2} alt="Man wearing black stussy hoodie"/>
+        <img src={image3} alt="Stussy denim jacket"/>
+        <img src={image4} alt="Man wearing white stussy shirt and brown pants"/>
+        <img src={image5} alt="Miniature eight ball sitting on countertop"/>
+      </div>
     </div>
   );
 }
