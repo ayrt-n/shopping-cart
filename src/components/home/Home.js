a71ff273895f9206de09371b854fcaaaeef8fcd8
ayrton <ayrton.parkinson1@gmail.com>
2022-10-08 08:18:41 -0400

    Fix syntax error

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -13,10 +13,10 @@ function Home() {
         <img src={image1} className="Showcase-item" alt="Man wearing black stussy shirt"/>
         <img src={image2} className="Showcase-item" alt="Man wearing black stussy hoodie"/>
       </div>
-        <div class="Home-showcase-container">
-          <img src={image3} className="Showcase-item" alt="Stussy denim jacket"/>
-          <img src={image4} className="Showcase-item" alt="Man wearing white stussy shirt and brown pants"/>
-        </div>
+      <div className="Home-showcase-container">
+        <img src={image3} className="Showcase-item" alt="Stussy denim jacket"/>
+        <img src={image4} className="Showcase-item" alt="Man wearing white stussy shirt and brown pants"/>
+      </div>
       <div className="Home-showcase-container">
         <img src={image5} className="Showcase-item full-size" alt="Miniature eight ball sitting on countertop"/>
       </div>
