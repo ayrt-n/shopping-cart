10125596f73f14892709bd37a2a7cf351f50cc99
ayrton <ayrton.parkinson1@gmail.com>
2022-10-08 08:31:04 -0400

    Replace cart prop with quantity

--- src/components/shop/CartActionButton.js
+++ src/components/shop/CartActionButton.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import cartIcon from './images/cart.svg';
 
-function CartActionButton({ cart }) {
+function CartActionButton({ quantity }) {
+  if (quantity > 9) {
+    quantity = '9+';
+  }
+
   return (
     <div className='Shop-cart-action-button'>
       <img src={cartIcon} alt='' />
       <div className='Shop-cart-count'>
-        {cart.length}
+        {quantity}
       </div>
     </div>
   );
