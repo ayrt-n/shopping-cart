2e731f4e93c968c572357eb65f12a6846952b675
ayrton <ayrton.parkinson1@gmail.com>
2022-10-10 11:11:22 -0400

    Add tests for conditional rendering of buttons and classes

--- src/components/shop/__tests__/Checkout.test.js
+++ src/components/shop/__tests__/Checkout.test.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
+import userEvent from '@testing-library/user-event';
 import Checkout from '../Checkout';
 
 // Setup mocks
@@ -9,21 +10,74 @@ jest.mock('../CheckoutTable', () => () => (
 ));
 
 describe('Checkout component', () => {
-  it('renders CheckoutTable when cart is not empty', () => {
-    const cart = ['fake product'];
+  describe('checkout table', () => {
+    it('renders CheckoutTable when cart is not empty', () => {
+      const cart = ['fake product'];
 
-    render(<Checkout cart={cart} />);
-    const checkoutTable = screen.queryByTestId('checkout-table');
+      render(<Checkout cart={cart} />);
+      const checkoutTable = screen.queryByTestId('checkout-table');
 
-    expect(checkoutTable).not.toBeNull();
+      expect(checkoutTable).not.toBeNull();
+    });
+
+    it('renders Cart is empty! when cart is empty', () => {
+      const cart = [];
+
+      render(<Checkout cart={cart} />);
+      const emptyCartMessage = screen.getByTestId('empty-cart');
+
+      expect(emptyCartMessage).toHaveTextContent('Cart is empty!');
+    });
   });
 
-  it('renders Cart is empty! when cart is empty', () => {
-    const cart = [];
+  describe('checkout buttons', () => {
+    it('renders checkout button when cart is not empty', () => {
+      const cart = ['fake product'];
 
-    render(<Checkout cart={cart} />);
-    const emptyCartMessage = screen.getByTestId('empty-cart');
+      render(<Checkout cart={cart} />);
+      const checkoutButton = screen.queryByRole('button', {name: /checkout/i});
 
-    expect(emptyCartMessage).toHaveTextContent('Cart is empty!');
+      expect(checkoutButton).not.toBeNull();
+    });
+
+    it('does not render checkout button when cart is empty', () => {
+      const cart = [];
+
+      render(<Checkout cart={cart} />);
+      const checkoutButton = screen.queryByRole('button', {name: /checkout/i});
+
+      expect(checkoutButton).toBeNull();
+    });
+
+    it('sends closeMenu on cancel button click', () => {
+      const cart = [];
+      const closeMenuMock = jest.fn();
+
+      render(<Checkout cart={cart} closeMenu={closeMenuMock} />);
+      const cancelButton = screen.getByRole('button', {name: /cancel/i});
+      userEvent.click(cancelButton);
+
+      expect(closeMenuMock).toHaveBeenCalled();
+    });
+  });
+
+  describe('menu active', () => {
+    it('has class hidden when menuActive is false', () => {
+      const cart = [];
+
+      render(<Checkout cart={cart} menuActive={false} />);
+      const checkout = screen.getByTestId('checkout');
+
+      expect(checkout).toHaveClass('hidden');
+    });
+
+    it('does not have class hidden when menuActive is true', () => {
+      const cart = [];
+
+      render(<Checkout cart={cart} menuActive={true} />);
+      const checkout = screen.getByTestId('checkout');
+
+      expect(checkout).not.toHaveClass('hidden');
+    });
   });
 });
