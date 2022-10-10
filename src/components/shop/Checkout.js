8472d59a26c3a5baef2738b8e28325baa3b3085e
ayrton <ayrton.parkinson1@gmail.com>
2022-10-10 09:12:40 -0400

    Create Checkout menu component for displaying checkout details

---
+++ src/components/shop/Checkout.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import CheckoutTable from './CheckoutTable';
+
+function Checkout({ cart, menuActive, closeMenu }) {
+  return (
+    <div className={menuActive ? "Shop-checkout" : "Shop-checkout hidden"}>
+      <h1>Shopping Cart</h1>
+      {cart.length === 0
+        ? <div className="Checkout-message">Cart is empty!</div>
+        : <CheckoutTable cart={cart} />
+      }
+      <div className="Checkout-buttons">
+        {cart.length !== 0 &&
+          <button className="Shop-checkout-button">Checkout</button>
+        }
+        <button className="Shop-checkout-button cancel" onClick={closeMenu}>Cancel</button>
+      </div>
+    </div>
+  );
+}
+
+export default Checkout
