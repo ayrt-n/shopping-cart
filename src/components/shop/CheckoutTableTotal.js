57eec04d5640ddc79a402de183fc11a2fdf4ddba
ayrton <ayrton.parkinson1@gmail.com>
2022-10-10 10:18:06 -0400

    Remove table/tbody wrapper

--- src/components/shop/CheckoutTableTotal.js
+++ src/components/shop/CheckoutTableTotal.js
@@ -4,14 +4,10 @@ function CheckoutTableTotal({ cart }) {
   const total = cart.reduce((sum, item) => sum + (item.quantity * item.price), 0);
 
   return (
-    <table>
-      <tbody>
-        <tr>
-          <th colSpan="2">Total</th>
-          <td data-testid="checkout-total">${total}</td>
-        </tr>
-      </tbody>
-    </table>
+    <tr>
+      <th colSpan="2">Total</th>
+      <td data-testid="checkout-total">${total}</td>
+    </tr>
   );
 }
 
